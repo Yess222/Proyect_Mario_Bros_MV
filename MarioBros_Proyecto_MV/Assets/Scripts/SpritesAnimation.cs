1ba4397ae282a2bc696c7b0dff4d8a2717ca5d11
Gino Flores <gino.alexisfl@gmail.com>
2024-05-11 13:10:03 -0500

    2:00:25
    Animation Flower and Coin.
    Create Block.

---
+++ MarioBros_Proyecto_MV/Assets/Scripts/SpritesAnimation.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpritesAnimation : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public float frameTime = 0.1f;
+
+    //float timer = 0f;
+    int animationFrame = 0;
+
+    SpriteRenderer spriteRenderer;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Animation();
+        StartCoroutine(Animation());
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= frameTime) 
+        {
+            //Cambiamos de sprites
+            //Sumamos 1 a nuestro indice
+            animationFrame++;
+            //Tenemos que controlar que el indice no es mayor
+            //que el numero de registros del array
+            //Si es así, significa que ya hemos pasado el último 
+            //y volvemos a empezar
+            if (animationFrame >= sprites.Length)
+            {
+                animationFrame = 0;
+            }
+
+            spriteRenderer.sprite = sprites[animationFrame];
+            timer = 0;
+        }
+    }*/
+
+    IEnumerator Animation()
+    {
+        //while(animationFrame < sprites.Length)
+        while (true)  
+        {
+            Debug.Log("Animation Frame: " + animationFrame);
+            spriteRenderer.sprite = sprites[animationFrame];
+            //yield return null; //que vuelva en el sgte frame            
+            animationFrame++;
+            if(animationFrame >= sprites.Length)
+            {
+                animationFrame = 0;
+            }
+            yield return new WaitForSeconds(frameTime);
+        }
+    }
+}
